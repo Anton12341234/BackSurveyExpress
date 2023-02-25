cb63d00a5ca076651e2dae51a0bdd9f437b14577
Anton12341234 <antoxapopovich@gmail.com>
2023-02-25 13:02:23 +0100

    done

--- index.js
+++ index.js
@@ -3,27 +3,20 @@ import mongoose from 'mongoose'
 import fileUpload from 'express-fileupload';
 import cors from 'cors';
 import router from './authRouter.js';
-import path from 'path'
-import filePath from './middlewaree/filePath.js';
-import { fileURLToPath } from 'url';
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const DB_URL = `mongodb+srv://anton:0932304567@cluster0.j8curid.mongodb.net/?retryWrites=true&w=majority`
 
 const app = express()
 
 const corsOptions = {
-  origin: "https://cryptic-atoll-47501.herokuapp.com/",
+  origin: "http://185.233.118.209:4000",
 };
 
-const __filename = fileURLToPath(import.meta.url);
-
-const __dirname = path.dirname(__filename);
 
 app.use(cors(corsOptions));
 
-app.use(filePath(path.resolve(__dirname, 'files')))
 app.use(express.json())
 app.use(express.static('static'))
 app.use(fileUpload({}))
--- middlewaree/filePath.js
+++
@@ -1,6 +0,0 @@
-export default function filePath (path){
-   return function (req, res, next) {
-        req.filePath = path
-        next();
-    }
-}
